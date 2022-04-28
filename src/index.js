b99c3170efbf548b1e12f7f4ab2d68aacd0d4b63
Moses <moussa.abdelghany@gmail.com>
2022-04-28 17:37:56 +0200

    i have fix the problem of running the app and install the last version of reacr and react-router using this command (npm i react@latest react-dom@latest)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 import React from 'react';
 //import reportWebVitals from './reportWebVitals';
-import {createRoot} from 'react-dom/client'
+// import {createRoot} from 'react-dom/client'
 
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+// import '../node_modules/bootstrap/dist/css/bo
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import './index.css';
 // import  'react-bootstrap';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import {BrowserRouter as Router} from "react-router-dom";
-const container = document.getElementById("root")
-const root = createRoot(root);
+// const container = document.getElementById("root")
+// const root = createRoot(root);
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <Router>
-      <App />
+<App tab="home" />
+
   </Router>
+
 );
 
+// import ReactDOM from "react-dom/client";
+// import reportWebVitals from "./reportWebVitals";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+
+//   <Router>
+//       <App />
+//   </Router>
+
+//   </React.StrictMode>
+// );
+// reportWebVitals();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
